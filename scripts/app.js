4d77be74211dd7d28fb7197d878bda8e949daa41
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-10-14 14:23:01 +0500

    Updating

--- scripts/app.js
+++ scripts/app.js
@@ -426,7 +426,7 @@ function addMaintenanceToggler(clicker) {
     // clicker.parentElement.style.boxShadow = "0px 3px 10px rgb(0 0 0 / 16%)";
     clicker.parentElement.style.height = "54px";
     clicker.parentElement.style.width = "43%";
-    wholeModal3.style.minHeight = "70vh !important"
+    wholeModal3.style.minHeight = "70vh"
     wholeModal3.style.transitionDuration = "1000ms"
   }
 }
