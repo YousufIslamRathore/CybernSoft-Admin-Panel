2d5923a8af00e5a6d24d3d916447ebf292b064ed
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-10-14 14:22:24 +0500

    Updating

--- scripts/app.js
+++ scripts/app.js
@@ -420,12 +420,13 @@ function addMaintenanceToggler(clicker) {
     // clicker.parentElement.style.boxShadow = "none";
     clicker.parentElement.style.height = "37%";
     clicker.parentElement.style.width = "84%";
-    // clicker.parentElement.style.position = "static"
     wholeModal3.style.minHeight = "90vh"
+    wholeModal3.style.transitionDuration = "1000ms"
   } else {
     // clicker.parentElement.style.boxShadow = "0px 3px 10px rgb(0 0 0 / 16%)";
     clicker.parentElement.style.height = "54px";
     clicker.parentElement.style.width = "43%";
-    wholeModal3.style.minHeight = "70vh"
+    wholeModal3.style.minHeight = "70vh !important"
+    wholeModal3.style.transitionDuration = "1000ms"
   }
 }
