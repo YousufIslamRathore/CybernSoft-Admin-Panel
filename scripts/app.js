50af9d811ac99115c405c1155931188b2da0ffe4
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-11-23 12:16:35 +0500

    Updating UI

--- scripts/app.js
+++ scripts/app.js
@@ -759,8 +759,3 @@ function loadNotificationData() {
 function shakeBtn(btn) {
   btn.className = "animate__animated animate__shakeX"
 }
-
-function consoleLog() {
-  var cl = console.log.bind(document);
-  cl("Hey! Is this console ?")
-}
